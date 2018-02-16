5a676b6142adb26e63944e6155dafa27d7fcd871
user <shalomfriss@gmail.com>
2018-02-16 17:11:49 -0600

    Bip32 is working

--- src/App.js
+++ src/App.js
@@ -59,10 +59,19 @@ class App extends Component {
     
     console.log("SEED: " + seed)
     
+    var testSeed = "000102030405060708090a0b0c0d0e0f"
+    //testSeed = Bip39.createSeed(testSeed, "")	
+    var priKey = "xprv9s21ZrQH143K3QTDL4LXw2F7HEK3wJUD2nW2nRk4stbPy6cq3jPPqjiChkVvvNKmPGJxWUtg6LnF5kejMRNNU3TGtRBeJgk33yuGBxrMPHi"
+    var pubKey = "xpub661MyMwAqRbcFtXgS5sYJABqqG9YLmC4Q1Rdap9gSE8NqtwybGhePY2gZ29ESFjqJoCu1Rupje8YtGqsefD265TMg7usUDFdp6W1EGMcet8"
     
-    var keys = Bip32.createMasterNode(seed)
-    console.log("KEYS")
-    console.log(keys)
+    var keys = Bip32.createMasterNode(testSeed)
+    
+    console.log("KEYS ++++++++++++++++++++++++++++++")
+    console.log(priKey)
+    console.log(keys.m)
+    console.log(pubKey)
+    console.log(keys.M)
+    
     console.log(Bip32.HIGH_BIT)
     //var keys = WalletUtil.createKeysFromSeed(seed)
     //console.log(keys)
--- src/bip/Bip32.js
+++ src/bip/Bip32.js
@@ -6,7 +6,7 @@ import crypto from 'crypto';
 import bs58 from 'bs58';
 import { CryptoUtil } from './../crypto/CryptoUtil';
 import { KeyGenerator } from './../crypto/KeyGenerator';
-
+import jsSHA from 'jssha';
 
 export class Bip32 {
 	
@@ -24,11 +24,10 @@ export class Bip32 {
 		 console.log("SEED: " + seed)
 		 
 		 //Take a sha512 hmac of the seed
-		var hmac = crypto.createHmac('sha512', '');
-		hmac.update(seed);
+		var hmac = crypto.createHmac('SHA512', 'Bitcoin seed');
+		hmac.update(seed, "hex");
 		var digest = hmac.digest('hex')
 		
-		//console.log("DIGEST: " + digest)
 		
 		//Split the digest into two parts
 		var masterPrivateKey = digest.substr(0, 64) 
@@ -37,6 +36,7 @@ export class Bip32 {
 		console.log("MASTER PRIVATE KEY: " + masterPrivateKey)
 		console.log("MASTER CHAIN CODE: " + masterChainCode)
 		
+		
 		//Calculate the master public key  
 		var theNumber = new sjcl.bn("0x" + masterPrivateKey) 
 		var K = sjcl.ecc.curves.k256.G.mult(theNumber)
@@ -44,11 +44,12 @@ export class Bip32 {
 		var yhex = sjcl.codec.hex.fromBits(K.y.toBits())
 		var masterPublicKey = {x:xhex, y:yhex}
 		
+		//Calc compressed pub key
 		var compressedPublicKey = KeyGenerator.generateCompressedBitcoinPublicKey(masterPrivateKey)
 		console.log("COMPRESSED PUBLIC KEY: " + compressedPublicKey)
+		
+		
 				
-		
-		
 		//Create base58 check representation of a compressed private key (01 added to the end)
 		var b58Private = "80" + masterPrivateKey + "01"
 		var b58PrivateChecksum =  CryptoUtil.getChecksum32(b58Private) 
@@ -59,23 +60,48 @@ export class Bip32 {
 		
 		
 		//Encode the private key
-		var str = "0488ADE4" + "00" + "00000000" + "00000000" + masterChainCode + "00" + masterPrivateKey
+		var str = "0488ade4" + "00" + "00000000" + "00000000" + masterChainCode + "00" + masterPrivateKey
 		var checksum = CryptoUtil.getChecksum32(str) 
 		str = str + checksum
 		var bytes = Buffer.from(str, 'hex')
-		var enc = bs58.encode(bytes)
-		console.log("--PRIVATE KEY ENCODED: " + enc + " - " + enc.length)
+		var enc1 = bs58.encode(bytes)
+		console.log("--PRIVATE KEY ENCODED: " + enc1 + " - " + enc1.length)
 		
 		
+		var test = "xpub661MyMwAqRbcFtXgS5sYJABqqG9YLmC4Q1Rdap9gSE8NqtwybGhePY2gZ29ESFjqJoCu1Rupje8YtGqsefD265TMg7usUDFdp6W1EGMcet8"
+		var dec1 = bs58.decode(test)
+		console.log("DECODED 1: " + dec1.toString('hex'))
+		//0488b21e000000000000000000
+		//873dff81c02f525623fd1fe5167eac3a55a049de3d314bb42ee227ffed37d508
+		//0339a36013301597daef41fbe593a02cc513d0b55527ec2df1050e2e8ff49c85c2
+		//ab473b21
+		
+		
+		//be5badd2e1fa21e07d888d94690ba40b8925127e716cb997daf1879fe96fce08
+		
+		//Mine decoded
+		//0488b21e000000000000000000
+		//0bb44f25c5c90bc2aff51bc70e7d9b38ad7cefe00723baea8627d9a199f84d0a
+		//03390b552177f0847ad0cbaf1faeeeb90b47f6716bc0b3ea4319e71067bde02b9a
+		//fe42fae6
+		
+		//xpub661MyMwAqRbcEfC95gxBgUisDZdSDQj6LBCaE3c5yEP3Kv8jrqm8ks5RPzMCt2XmvSxQChuDz3tq3bdp8kxQx29uenX4cHePYVEactuAFrD
+		
+		//be5badd2e1fa21e07d888d94690ba40b8925127e716cb997daf1879fe96fce08
 		//Public key creation - xpub661MyMwAqRbcGdRF7Aa79CJLzUM6gw7jd3knnCZJfqb6oWaC598xgV96feHqKUWxjXQ6yUYaZW4yMaCKyCYYz2V5xJaB6X6E6AYxxreDeUD
-		var publicStr = "0488B21E" + "00" + "00000000" + "00000000" + masterChainCode + compressedPublicKey
+		var publicStr = "0488b21e" + "00" + "00000000" + "00000000" + masterChainCode + compressedPublicKey
+		
 		var publicChecksum = CryptoUtil.getChecksum32(publicStr)
 		publicStr = publicStr + publicChecksum
+		console.log("STR: " + publicStr)
 		bytes = Buffer.from(publicStr, 'hex')
-		enc = bs58.encode(bytes)
+		var enc = bs58.encode(bytes)
 		console.log("--PUBLIC KEY ENCODED: " + enc + " - " + enc.length)
 		
-		return {m: masterPrivateKey, c: masterChainCode, M: masterPublicKey}
+		
+		//return {m: masterPrivateKey, c: masterChainCode, M: masterPublicKey}
+		return {m: enc1, M: enc}
+		
 		
 	}
 }
